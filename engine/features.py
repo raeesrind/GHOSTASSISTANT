ed126201fbf7bc1cd210968788bfd2bdc53103f9
raeesrind <jrrais2003@gmail.com>
2025-07-02 08:59:31 +0200

    part 3

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\jarvis_advance_sound-[AudioTrimmer.com] (1).mp3"
+    playsound(music_dir)
--- www/main.py
+++ www/main.py
@@ -1,8 +1,16 @@
+import sys
 import os
-
 import eel
 
+# Add root directory (GHOST/) to sys.path so we can import engine/
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from engine.features import *
+
 eel.init("www")
+
+playAssistantSound()
+
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
